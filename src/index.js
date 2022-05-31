ea8846d6c9aa8ad1b0aab4f4f64edd72eaae33f0
khalil <khalillakhdharatc@gmail.com>
2022-05-31 13:42:23 +0100

    integrationReact

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+ReactDOM.render(
+<Home />, document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
 module.exports = {
 entry: path.resolve(__dirname, './src/index.js'),
-output: {
-path: path.resolve(__dirname, './public'),
-filename: 'bundle.js',
+module: {
+rules: [
+{
+test: /\.(js|jsx)$/,
+exclude: /node_modules/,
+use: ['babel-loader'],
 },
-};
+],
+},
+resolve: {
+extensions: ['*', '.js', '.jsx'],
+},
+output: {
+path: path.resolve(__dirname, './public'), filename: 'bundle.js', },}
