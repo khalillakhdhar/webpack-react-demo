6a2a9f27944ed27beda2442dbf035475c21085f9
khalil <khalillakhdharatc@gmail.com>
2022-05-31 14:46:38 +0100

    imcinitial

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
-const Home = () => {
-return <div>React Application</div>
+class Home extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state=
+        {
+            taille:'',
+            poid:'',
+            indice:'',
+            remarque:''
+
+        };
+        this.onChange=this.onChange.bind(this);
+        this.reset=this.reset.bind(this);
+    }
+    onChange = e =>
+    {
+        this.setState({ [e.target.name] : e.target.value});
+    }
+    reset()
+    {
+        this.setState({
+            taille:'',
+            poid:'',
+            indice:'',
+            remarque:''
+        })
+    }
+render()
+{
+    return(
+<div><h1>Home</h1></div>
+    );
 }
+    }
 export default Home
